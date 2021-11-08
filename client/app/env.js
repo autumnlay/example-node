ce03a95cc6a6919bc3797f28b154083176ca0f78
Autumn Lay <autumn.lay0@gmail.com>
2021-11-08 08:12:10 -0700

    endofday

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'autumn-codeworks.us.auth0.com'
+export const audience = 'https://autumn-bcw.com'
+export const clientId = 'BFC81E6RCEnEND7yqGET5O5R7UGhwmmo'
